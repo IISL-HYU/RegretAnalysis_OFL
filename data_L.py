cd949dba7faebdd9084b6ca2323905394a0af644
DohyeokKwon <dohyeokkwon@Dohyeoks-iMac.local>
2023-07-11 19:08:36 +0900

    230711 19:08

--- data_L.py
+++ data_L.py
@@ -1,5 +1,13 @@
 import random
+import numpy as np
 import tensorflow as tf
+
+def MNIST_data(iid = True, shuffle = False):
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+    x_train = x_train.reshape((60000, 28, 28, 1))
+    x_train = x_train / 255.0
+
+    return 'MNIST', x_train, y_train, -1
 
 def CIFAR_10_data():
     (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
@@ -12,15 +20,19 @@ def EMNIST_data():
     return 
 
 def data_shuffle(x_train, y_train):
-    tmp = [[x,y] for x, y in zip(x_train, y_train)]
+    tmp = list(zip(x_train, y_train))
     random.shuffle(tmp)
-    x_train = [n[0] for n in tmp]
-    y_train = [n[1] for n in tmp]
-    
-    return x_train, y_train
+    x_train, y_train = zip(*tmp)
+
+    return np.array(x_train), y_train
     
     
 if __name__ == '__main__':
-    x, y, size = CIFAR_10_data()
-    print(x[0])
-    print(y[0])
+    name, x_t, y_t, size = MNIST_data()
+    print(type(x_t), type(x_t[0]))
+    print(x_t[0:1].shape)
+    print(y_t[0])
+    x_t, y_t = data_shuffle(x_t, y_t)
+    print(type(x_t), type(x_t[0]))
+    print(x_t[0:1].shape)
+    print(y_t[0])
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+import time
 import pickle
 import numpy as np
 
-from data_L import CIFAR_10_data, data_shuffle
+from data_L import CIFAR_10_data, MNIST_data, data_shuffle
 from model import OFL_Model
 
 K = 100        # Number of clients
-D = 52874       # CIFAR-10
+D = 34826 #52874       # CIFAR-10
 P = 0.10        # Com. overhead reduction rate from FedOGD
 
 def opt_param(p1, D, print_result):
@@ -35,18 +36,18 @@ def opt_param(p1, D, print_result):
     return min_index, min_alpha, int(min_alpha * D), min_p2
 s, _, b, p = opt_param(P, D, True)
 
-data, x_train, y_train, input_size = CIFAR_10_data() #MNIST_data() #Room_data()
+data, x_train, y_train, input_size = MNIST_data() #Room_data() CIFAR_10_data() #
 task = 'clf'
 
 Model_list = []
 Model_list.append(OFL_Model('FedOGD', task, K, [False, 0, 0], 1, 1, input_size))
-Model_list.append(OFL_Model('OFedAvg', task, K, [False, 0, 0], P, 1, input_size))
-Model_list.append(OFL_Model('FedOMD', task, K, [False, 0, 0], 1, int(1/P), input_size))
-Model_list.append(OFL_Model('OFedIQ', task, K, [True, s, b], p, 1, input_size))
+# Model_list.append(OFL_Model('OFedAvg', task, K, [False, 0, 0], P, 1, input_size))
+# Model_list.append(OFL_Model('FedOMD', task, K, [False, 0, 0], 1, int(1/P), input_size))
+# Model_list.append(OFL_Model('OFedIQ', task, K, [True, s, b], p, 1, input_size))
 print("========= Model_list is generated ===================")
 print()
 
-iter_max = 1
+iter_max = 2
 i_max = len(y_train) // K
 print()
 print("Total timesteps :", iter_max*i_max, "| Data reuse :", iter_max, "| steps per dataset :", i_max)
@@ -55,11 +56,11 @@ print()
 for model in Model_list:
     print("==========", model.name, "===========================")
     for iter in range(iter_max):
-        #x_train, y_train = data_shuffle(x_train, y_train)
+        x_train, y_train = data_shuffle(x_train, y_train)
         for i in range(i_max):
             model.train(x_train[K*i : K*(i+1)], y_train[K*i : K*(i+1)], ((i_max * iter) + (i+1)) % model.L)
         last_acc = model.pull_last_result()
-        print("iter",iter," | Accuracy =", last_acc, "%")
+        print("iter",iter," | Accuracy =", last_acc, "% | Time =", time.ctime(time()))
     print("==========", model.name, "finished ==================")
     
     result = model.pull_result()
